f787a107e4d5e2e9131368eca4418b5edac237a3
kelar crisp <kelcrisp007@icloud.com>
2019-06-14 16:42:00 -0700

    final commit with only 0,1,2,3 working

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,48 @@
 
-
 //user
 $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
-    var userInput = (parseInt($("input#number").val()));
-    $("#output").text(output);
-debugger;
-function beeps(userInput){
-var dave = "I'm sorry Dave, I'm afraid I can't do that.";
-var boop = "boop";
-var beep = "beep";
+    var userInput = parseInt($("input#number").val());
 
-var myArray= [];
+    $("#output").text(userInput);
+    myFunction(userInput);
 
-for (var i = 0; i <= userInput; i++){
-  if (userInput === "") {
-    return NaN;
-  } else if ([i].includes(3)){
-    myArray.push(message);
 
-  } else if ([i].includes(2)){
-    myArray.push(boop);
 
-  } else if ([i].includes(1)){
-    myArray.push(beep);
 
-  else if ([i].includes(0)){
-  myArray.push(0);
+//buisness
+function myFunction(userInput){
+  var dave = "I'm sorry Dave, I'm afraid I can't do that.";
+  var beep = "beep";
+  var boop = "boop";
+  var myArray =[];
+  for (var i = 0; i <= userInput; i++){
+
+    if (userInput === "") {
+      return NaN;}
+
+        for (var i = 0; i <= userInput; i++){
+          if (userInput === ""){
+            return NaN;
+          } else if ([i].includes(3)){
+            myArray.push(dave);
+
+          } else if ([i].includes(2)){
+            myArray.push(boop);
+
+          } else if ([i].includes(1)){
+            myArray.push(beep);
+
+          }  else if ([i].includes(0)){
+            myArray.push(0);
+          }
+
+
+          }
+        }
+
+    console.log(myArray)
     }
-  }
-
-
-
-}
   });
 });
