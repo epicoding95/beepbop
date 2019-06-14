b221a73ded8d752c1954b27a9be5d44ad5f92a98
kelar crisp <kelcrisp007@icloud.com>
2019-06-14 10:41:19 -0700

    basic question and answer made

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
+
+//user logic
+
 $(document).ready(function() {
 $('form').submit(function(event) {
   event.preventDefault();
-  var userInput = ("input#number").val();
-  $("#output").text(output);
+var userInput = $("input#number").val();
+var myArray =[];
+$("#output").text(userInput);
+
+
+
+
+// buisness logic
+debugger;
+
+
+
+for (var i = 0; i<=userInput; i++; ){
+
+}
+
+
+
+if (userInput === "john"){
+
+$("#output").text(john)
+
+};
+
+
+
 });
 });
