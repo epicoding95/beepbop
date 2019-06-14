c6701aad94fe6f1fa7c36468ac21ff0fe85679c7
kelar crisp <kelcrisp007@icloud.com>
2019-06-14 14:58:09 -0700

    working if statement for 1,2,3

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,35 @@ $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#number").val());
-    var myArray =[];
+
     $("#output").text(userInput);
-
-debugger;
-
-
-  myFunction(userInput);
-
-
-
-
-
-
-
+    myFunction(userInput);
 
   });
 });
 
-  function myFunction(userInput){
+function myFunction(userInput){
   var dave = "I'm sorry Dave, I'm afraid i can't do that.";
   var beep = "beep";
   var boop = "boop";
-
-
   var myArray =[];
-
   for (var i = 0; i <= userInput; i++){
 
-  if (userInput === "") {
-      return enter a number;
-    } else if (str(i).includes(3)) {
+    if (userInput === "") {
+      return NaN;
+    } else if ([i].includes(3)) {
       myArray.push(dave);
-
-
+    }
+    else if ([i].includes(2)) {
+      myArray.push(boop)
   }
-    console.log(myArray.toString());
+  else if ([i].includes(1)) {
+    myArray.push(beep)
 }
+
+
+}
+console.log(myArray);
+return (myArray);
+
 }
