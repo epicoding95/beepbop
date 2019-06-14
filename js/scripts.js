1c161abc73261f0ec4e042ac7da4f4a9fc5ff772
kelar crisp <kelcrisp007@icloud.com>
2019-06-14 16:09:17 -0700

    trial

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,38 @@
 
-//user logic
 
+//user
 $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("input#number").val());
+    var userInput = (parseInt($("input#number").val()));
+    $("#output").text(output);
+debugger;
+function beeps(userInput){
+var dave = "I'm sorry Dave, I'm afraid I can't do that.";
+var boop = "boop";
+var beep = "beep";
 
-    $("#output").text(userInput);
-    myFunction(userInput);
+var myArray= [];
 
+for (var i = 0; i <= userInput; i++){
+  if (userInput === "") {
+    return NaN;
+  } else if ([i].includes(3)){
+    myArray.push(message);
+
+  } else if ([i].includes(2)){
+    myArray.push(boop);
+
+  } else if ([i].includes(1)){
+    myArray.push(beep);
+
+  else if ([i].includes(0)){
+  myArray.push(0);
+    }
+  }
+
+
+
+}
   });
 });
-
-function myFunction(userInput){
-  var dave = "I'm sorry Dave, I'm afraid i can't do that.";
-  var beep = "beep";
-  var boop = "boop";
-  var myArray =[];
-  for (var i = 0; i <= userInput; i++){
-
-    if (userInput === "") {
-      return NaN;
-    } else if ([i].includes(3)) {
-      myArray.push(dave);
-    }
-    else if ([i].includes(2)) {
-      myArray.push(boop)
-    }
-    else if ([i].includes(1)) {
-    myArray.push(beep)
-    }
-    else if ([i].includes(0)) {
-    myArray.push(0)
-
-}
-
-}
-console.log(myArray);
-
-}
