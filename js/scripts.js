1c0510a71d934ab63eb5c6a9d4dd085b473de921
kelar crisp <kelcrisp007@icloud.com>
2019-06-14 13:11:05 -0700

    still a broken loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,37 @@
 //user logic
 
 $(document).ready(function() {
-$('form').submit(function(event) {
-  event.preventDefault();
-var userInput = $("input#number").val();
-var myArray =[];
-$("#output").text(userInput);
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#number").val());
+    var myArray =[];
+    $("#output").text(userInput);
 
 
 
 
-// buisness logic
-debugger;
+    // buisness logic
+    var dave = "I'm sorry Dave, I'm afraid i can't do that.";
+    var beep = "beep";
+    var boop = "boop";
+
+    debugger;
+    var myArray = [];
+    for (var i = 0; i < 10; i++){
+
+    }
+    // for (var i = 0; i <= userInput; i++){
+    //   if (userInput(3)){
+    //     myArray.splice(userInput(i, 1, dave));
+    //   }
+    // }
 
 
 
-for (var i = 0; i<=userInput; i++; ){
-
-}
 
 
 
-if (userInput === "john"){
-
-$("#output").text(john)
-
-};
 
 
-
-});
+  });
 });
